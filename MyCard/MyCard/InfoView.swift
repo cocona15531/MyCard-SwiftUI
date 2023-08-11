4b3f02e9c22cb90e9dcbeb36fe8d61128e4e6db0
issei.ueda <ron.ron633662@gmail.com>
2023-08-11 11:17:20 +0900

    first commit

---
+++ MyCard/MyCard/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  MyCard
+//
+//  Created by Issei Ueda on 2023/08/10.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
